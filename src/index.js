e636a903b45261ecba7498ae314a83bf6c3a9d74
LiMengwow <406351638@qq.com>
2018-08-28 10:08:17 +0800

    测试版：0.0.2

--- src/index.js
+++ src/index.js
@@ -1,49 +1,76 @@
-function modConsole(){
-    var div = document.createElement('div');
-    div.className = 'mod-console';
-    document.body.appendChild(div);
-    var div2 = document.createElement('div');
-    div2.className = 'mod-open';
-    div2.innerHTML = '开启'
-    document.body.appendChild(div2);
-}
-modConsole()
-function dataOut(obj, name){
-    if(!obj) return false;
-    var str = name ? (name + ' : ' + obj) : obj;
-    var divItem = document.createElement('div');
-    divItem.className = 'mod-console-item';
-    divItem.innerHTML = str;
-    document.getElementsByClassName('mod-console')[0].appendChild(divItem)
-}
-dataOut('5445')
-
-var modOpen = document.getElementsByClassName('mod-open')[0];
-var modWrapper = document.getElementsByClassName('mod-console')[0];
-var startY, divHeight;
-modOpen.onclick = function(){
-    modWrapper.style.display = 'block';
-}
-var timeOutEvent;
-modWrapper.addEventListener('touchstart', function(e){
-    timeOutEvent = setTimeout(function() {
-        timeOutEvent = 0;
-        modWrapper.style.display = 'none';
-    }, 350);
-    startY = e.touches[0].pageY
-    divHeight = modWrapper.offsetHeight;
-})
-
-modWrapper.addEventListener('touchmove', function(e){
-    clearTimeout(timeOutEvent);
-    timeOutEvent = 0;
-    modWrapper.style.height = divHeight - e.touches[0].pageY + startY + 'px'
-})
-
-modWrapper.addEventListener('touchend', function(){
-    clearTimeout(timeOutEvent);
-    if (timeOutEvent != 0) {
-        console.log('你点击了')
+class WebConsole{
+    constructor(){
+        this.num = 0;
+        this.modConsole()
+        this.modOpen = document.getElementsByClassName('mod-open')[0];
+        this.modWrapper = document.getElementsByClassName('mod-console')[0];
+        this.startY
+        this.divHeight
+        this.openClick()
+        this.timeOutEvent
+        this.modTouchstart()
+        this.modTouchmove()
+        this.modTouchend()
     }
-    return false;
-})
+    modConsole(){
+        var div = document.createElement('div');
+        div.className = 'mod-console';
+        document.body.appendChild(div);
+        var div2 = document.createElement('div');
+        div2.className = 'mod-open';
+        div2.innerHTML = '开启'
+        document.body.appendChild(div2);
+    }
+    dataOut(obj, name){
+        if(!obj) return false;
+        this.num++;
+        var str = name ? (this.num + ' : ' + name + ' : ' + obj) : this.num + ' : ' + obj;
+        var divItem = document.createElement('div');
+        divItem.className = 'mod-console-item';
+        divItem.innerHTML = str;
+        document.getElementsByClassName('mod-console')[0].appendChild(divItem)
+    }
+    openClick(){
+        var that = this;
+        that.modOpen.onclick = function(){
+            if(that.modOpen.innerHTML == '开启'){
+                that.modWrapper.style.display = 'block';
+                that.modOpen.innerHTML = '关闭'
+            } else {
+                that.modWrapper.style.display = 'none';
+                that.modOpen.innerHTML = '开启'
+            }
+        }
+    }
+    modTouchstart(){
+        var that = this;
+        that.modWrapper.addEventListener('touchstart', function(e){
+            that.timeOutEvent = setTimeout(function() {
+                that.timeOutEvent = 0;
+                that.dataOut('你长按了')
+            }, 350);
+            that.startY = e.touches[0].pageY
+            that.divHeight = that.modWrapper.offsetHeight;
+        })
+    }
+    modTouchmove(){
+        var that = this;
+        that.modWrapper.addEventListener('touchmove', function(e){
+            clearTimeout(that.timeOutEvent);
+            that.timeOutEvent = 0;
+            that.modWrapper.style.height = that.divHeight - e.touches[0].pageY + that.startY + 'px'
+        })
+    }
+    modTouchend(){
+        var that = this;
+        that.modWrapper.addEventListener('touchend', function(){
+            clearTimeout(that.timeOutEvent);
+            if (that.timeOutEvent != 0) {
+                that.dataOut('你点击了')
+            }
+            return false;
+        })
+    }
+}
+var cons = new WebConsole();
+cons.dataOut('222')
